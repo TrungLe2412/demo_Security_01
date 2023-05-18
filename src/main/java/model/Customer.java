ee17e54f0f1fa119758b52987e61765f6d7ee270
unknown <trungle2001yf@gmail.com>
2023-05-18 22:51:25 +0700

    creat customer

---
+++ src/main/java/model/Customer.java
@@ -0,0 +1,19 @@
+package model;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+
+public class Customer {
+    private String id;
+    private String name;
+    private String phoneNumber;
+    private String email;
+
+
+
+}
